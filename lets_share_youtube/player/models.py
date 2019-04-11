77485ff948819bcd4e15a7816b9f623fb658c56e
Hugo LEVY-FALK <klafyvel@gmail.com>
2019-04-11 13:21:02 +0200

    Add playlist and video models.

--- lets_share_youtube/player/models.py
+++ lets_share_youtube/player/models.py
@@ -1,3 +1,61 @@
-from django.db import models
+import random
+import json
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.conf import settings
+
+import requests
+
+
+class PlayList(models.Model):
+    """A PlayList, i.e. a collection of videos"""
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    public = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    token = models.CharField(blank=True, unique=True, null=True, max_length=100)
+    last_update = models.DateTimeField(auto_now=True)
+    last_get = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u"Playlist " + str(self.token)
+
+    def create_token(self):
+        """Creates a token for the playlist if it hasn't been done yet."""
+        if self.token is None:
+            self.token = str(
+                int(self.date.timestamp()) << settings.RANDOM_LENGTH
+                | random.randint(0, 2 ** settings.RANDOM_LENGTH - 1)
+            )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.create_token()
+        super().save(update_fields=["token"])
+
+    @property
+    def is_anonymous(self):
+        return self.owner is None
+
+
+class Video(models.Model):
+    """Represents a YouTube video."""
+
+    playlist = models.ForeignKey(PlayList, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100)
+    title = models.CharField(blank=True, max_length=100)
+
+    def __unicode__(self):
+        return u"Link : " + self.token + u" of " + str(self.playlist)
+
+    def save(self, *args, **kwargs):
+        self.update_title()
+        super().save(*args, **kwargs)
+
+    def update_title(self):
+        """Fetch the title of the video on YouTube using its token."""
+        response = requests.get(settings.YOUTUBE_INFO_URL.format(self.token))
+        self.title = json.loads(response.content.decode("utf-8"))["title"]
--- lets_share_youtube/player/tests.py
+++ lets_share_youtube/player/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import PlayList, Video
+
+
+class PlayListTestCase(TestCase):
+    """Tests for PlayList."""
+
+    def setUp(self):
+        PlayList.objects.create(title="Test playlist")
+
+    def test_token_already_created(self):
+        """A token should be added if, and only if, no one has been set before."""
+        already_created = PlayList.objects.get(title="Test playlist")
+        token = "plop"
+        already_created.token = token
+        already_created.save()
+        already_created = PlayList.objects.get(title="Test playlist")
+        self.assertEqual(already_created.token, token)
+
+    def test_token_not_set(self):
+        """A token should be added if, and only if, no one has been set before."""
+        playlist = PlayList.objects.create(title="Another test playlist")
+        self.assertIsNotNone(playlist.token)
+
+
+class VideoTestCase(TestCase):
+    """Tests for Videos"""
+
+    def setUp(self):
+        p = PlayList.objects.create(title="Test playlist")
+        Video.objects.create(token="QH2-TGUlwu4", playlist=p)
+
+    def test_title_is_fetched(self):
+        """The title of the video should be retrieved from YouTube when saved."""
+        v = Video.objects.get(token="QH2-TGUlwu4")
+        self.assertEqual("Nyan Cat [original]", v.title)
